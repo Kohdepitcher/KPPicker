1f2daf37500c65a7e9f04052c93602a8cc311359
Kohde Pitcher <kohde1@hotmail.com>
2022-03-15 00:18:14 +1000

    Created demo app for UI component

---
+++ Example/KPPicker Demo/KPPicker Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KPPicker Demo
+//
+//  Created by Kohde Pitcher on 15/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/KPPicker Demo/KPPicker Demo/KPPicker_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPPicker_DemoApp.swift
+//  KPPicker Demo
+//
+//  Created by Kohde Pitcher on 15/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct KPPicker_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
